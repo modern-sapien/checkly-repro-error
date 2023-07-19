733bbbd900a2d1553f7a9ec7ed3123298e5f9fee
jonathan <joncanales2@gmail.com>
2023-07-19 15:29:41 -0400

    added more checks and adjusted how the timeout was set to ensure it would fire and recorded results.

--- src/__checks__/api.check.ts
+++ src/__checks__/api.check.ts
@@ -1,84 +1,35 @@
 import * as path from 'path'
-import { ApiCheck, AssertionBuilder } from 'checkly/constructs'
+import { ApiCheck, AssertionBuilder, CheckGroup } from 'checkly/constructs'
 import { websiteGroup } from './website-group.check'
 
-new ApiCheck('homepage-api-check-1', {
-  name: 'Fetch Book List 1',
-  group: websiteGroup,
-  degradedResponseTime: 10000,
-  maxResponseTime: 20000,
-  setupScript: {
-    entrypoint: path.join(__dirname, './utils/setup.ts')
-  },
-  request: {
-    url: 'https://danube-web.shop/api/books',
-    method: 'GET',
-    followRedirects: true,
-    skipSSL: false,
-    assertions: [
-      AssertionBuilder.statusCode().equals(200),
-      AssertionBuilder.jsonBody('$[0].id').isNotNull(),
-    ],
-  }
-})
+function createApiChecks(websiteGroup: CheckGroup) {
+  let apiChecks = [];
+  
+  for (let i = 1; i <= 8; i++) {
+    let apiCheck = new ApiCheck(`homepage-api-check-${i}`, {
+      name: `Fetch Book List ${i}`,
+      group: websiteGroup,
+      degradedResponseTime: 10000,
+      maxResponseTime: 20000,
+      setupScript: {
+        entrypoint: path.join(__dirname, './utils/setup.ts')
+      },
+      request: {
+        url: `https://danube-web.shop/api/books/${i}`,
+        method: 'GET',
+        followRedirects: true,
+        skipSSL: false,
+        assertions: [
+          AssertionBuilder.statusCode().equals(200),
+          AssertionBuilder.jsonBody('$[0].id').isNotNull(),
+        ],
+      }
+    });
 
-
-new ApiCheck('homepage-api-check-2', {
-  name: 'Fetch Book List 2',
-  group: websiteGroup,
-  degradedResponseTime: 10000,
-  maxResponseTime: 20000,
-  setupScript: {
-    entrypoint: path.join(__dirname, './utils/setup.ts')
-  },
-  request: {
-    url: 'https://danube-web.shop/api/books',
-    method: 'GET',
-    followRedirects: true,
-    skipSSL: false,
-    assertions: [
-      AssertionBuilder.statusCode().equals(200),
-      AssertionBuilder.jsonBody('$[0].id').isNotNull(),
-    ],
+    apiChecks.push(apiCheck);
   }
-})
+  
+  return apiChecks;
+}
 
-new ApiCheck('homepage-api-check-3', {
-  name: 'Fetch Book List 3',
-  group: websiteGroup,
-  degradedResponseTime: 10000,
-  maxResponseTime: 20000,
-  setupScript: {
-    entrypoint: path.join(__dirname, './utils/setup.ts')
-  },
-  request: {
-    url: 'https://danube-web.shop/api/books',
-    method: 'GET',
-    followRedirects: true,
-    skipSSL: false,
-    assertions: [
-      AssertionBuilder.statusCode().equals(200),
-      AssertionBuilder.jsonBody('$[0].id').isNotNull(),
-    ],
-  }
-})
-
-new ApiCheck('homepage-api-check-4', {
-  name: 'Fetch Book List 4',
-  group: websiteGroup,
-  degradedResponseTime: 10000,
-  maxResponseTime: 20000,
-  setupScript: {
-    entrypoint: path.join(__dirname, './utils/setup.ts')
-  },
-  request: {
-    url: 'https://danube-web.shop/api/books',
-    method: 'GET',
-    followRedirects: true,
-    skipSSL: false,
-    assertions: [
-      AssertionBuilder.statusCode().equals(200),
-      AssertionBuilder.jsonBody('$[0].id').isNotNull(),
-    ],
-  }
-})
+createApiChecks(websiteGroup)
--- src/__checks__/utils/auth-client.ts
+++ src/__checks__/utils/auth-client.ts
@@ -1,10 +1,10 @@
 export async function getToken (): Promise<string> {
   console.log('Fetching a token from an imaginary auth API endpoint')
   
-  setTimeout(() => {
-    throw new Error();
-  }, 5000);
+  await new Promise(resolve => setTimeout(resolve, 15000));
+  console.log(new Date().toISOString())
 
   const token: string = await new Promise(resolve => { return resolve('123abc') })
   return token
 }
+
